17204fcc1dec4cc4c5af643e83724f319b4dbcd6
Chintan Patel <patelchintan843@gmail.com>
2023-01-25 22:17:20 +0530

    v53 (update note)

--- Back-End/routes/notes.js
+++ Back-End/routes/notes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const { useInsertionEffect } = require("react");
 var fetchuser = require('../middleware/fetchuser');
 const Note = require('../models/Note');
 
@@ -41,6 +42,30 @@ router.post('/addnote', fetchuser, [
         console.log(error.message)
         res.status(500).send("Internal server error")
     }
+})
+
+// Route3: Update an existing Note using: POST "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+
+    const { title, description, tag } = req.body;
+
+    // Create a newNote object
+    const newNote = {};
+    if (title) { newNote.title = title };
+    if (description) { newNote.description = description };
+    if (tag) { newNote.tag = tag };
+
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+        return res.status(404).send("Not Found")
+    }
+    if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not Allowed");
+    }
+    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+    res.json(note);
 })
 
 module.exports = router
