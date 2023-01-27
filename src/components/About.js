e65a0f8b7bbdfab32c5aee0e0d39df786162bc9d
Chintan Patel <patelchintan843@gmail.com>
2023-01-27 21:14:30 +0530

    v59 (location hook, create a html form)

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,19 @@
-import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
+import React from 'react'
+// import { useContext, useEffect } from 'react'
+// import noteContext from '../context/notes/noteContext'
 
 const About = () => {
-    const a = useContext(noteContext);
-    // Call the update function using useEffect method
-    useEffect(() => {
-        a.update()
-        // eslint-disable-next-line
-    }, [])
+    // const a = useContext(noteContext);
+    // // Call the update function using useEffect method
+    // useEffect(() => {
+    //     a.update()
+    //     // eslint-disable-next-line
+    // }, [])
     return (
         <div>
             {/* Pass the value using contextAPI */}
-            My name is {a.state.name} and i am in class {a.state.class}.
+            {/* My name is {a.state.name} and i am in class {a.state.class}. */}
+            About page
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,25 +1,27 @@
 import NoteContext from "./noteContext";
-import { useState } from "react";
+// import { useState } from "react";
+
 const NoteState = (props) => {
-    // Default value of context
-    const s1 = {
-        "name": "chintan",
-        "class": "5b"
-    }
-    const [state, setState] = useState(s1);
-    // Update the value after one second
-    const update = () => {
-        // Logic build of update the value after one second
-        setTimeout(() => {
-            setState({
-                "name": "patel",
-                "class": "6b"
-            })
-        }, 1000)
-    }
+    // // Default value of context
+    // const s1 = {
+    //     "name": "chintan",
+    //     "class": "5b"
+    // }
+    // const [state, setState] = useState(s1);
+    // // Update the value after one second
+    // const update = () => {
+    //     // Logic build of update the value after one second
+    //     setTimeout(() => {
+    //         setState({
+    //             "name": "patel",
+    //             "class": "6b"
+    //         })
+    //     }, 1000)
+    // }
     return (
         // Pass the props for use state and update
-        <NoteContext.Provider value={{ state, update }}>
+        // <NoteContext.Provider value={{ state, update }}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
