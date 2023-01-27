e6e9e3b928e9e0569886c255ef6988aeefc47d9c
Chintan Patel <patelchintan843@gmail.com>
2023-01-28 00:32:45 +0530

    v62 (fontawesome add)

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Navbar from './components/Navbar';
 import { Home } from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
+import Alert from './components/Alert';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
+          <Alert message="This is an amazing" />
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -10,6 +10,9 @@ const Noteitem = (props) => {
                     {/* show the title of record and description in card */}
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
+                    {/* Font awesome website use to get the icon */}
+                    <i className="fa-solid fa-trash mx-2"></i>
+                    <i className="fa-regular fa-pen-to-square mx-2"></i>
                 </div>
             </div>
         </div>
