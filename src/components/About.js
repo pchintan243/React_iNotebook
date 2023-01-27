9eed56026769ac6e92c2c9dcdbd007524b8821fd
Chintan Patel <patelchintan843@gmail.com>
2023-01-27 20:51:18 +0530

    v58 (use context hook)

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import noteContext from '../context/notes/noteContext'
 
 const About = () => {
+    const a = useContext(noteContext);
+    // Call the update function using useEffect method
+    useEffect(() => {
+        a.update()
+        // eslint-disable-next-line
+    }, [])
     return (
         <div>
-            About file
+            {/* Pass the value using contextAPI */}
+            My name is {a.state.name} and i am in class {a.state.class}.
         </div>
     )
 }
---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,28 @@
+import NoteContext from "./noteContext";
+import { useState } from "react";
+const NoteState = (props) => {
+    // Default value of context
+    const s1 = {
+        "name": "chintan",
+        "class": "5b"
+    }
+    const [state, setState] = useState(s1);
+    // Update the value after one second
+    const update = () => {
+        // Logic build of update the value after one second
+        setTimeout(() => {
+            setState({
+                "name": "patel",
+                "class": "6b"
+            })
+        }, 1000)
+    }
+    return (
+        // Pass the props for use state and update
+        <NoteContext.Provider value={{ state, update }}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState;
